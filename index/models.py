5563f88b9f48c0df52382507f14fe206bf969260
chilusoft <mukukachilu@gmail.com>
2024-12-07 18:56:13 +0200

    Setting the Abstract User Model

--- index/models.py
+++ index/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from djangondor.models import BaseTimestampModel, NULLABLE, NULLABLE_CHARFIELD
+from django.db.models import (
+    CharField,
+    IntegerField,
+    FloatField,
+    ForeignKey,
+    ManyToManyField,
+    OneToOneField,
+    EmailField
+)
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email=EmailField(max_length=255,unique=True)
+
