0960eb5b125e3a3472e0d59ca153a0351965598a
chilusoft <mukukachilu@gmail.com>
2024-12-08 10:49:22 +0200

    Added Product Models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from djangondor.models import BaseTimestampModel, NULLABLE, NULLABLE_CHARFIELD
+from django.db.models import (
+    CharField,
+    IntegerField,
+    FloatField,
+    ForeignKey,
+    ImageField,
+    DateField
+)
 
-# Create your models here.
+
+class Product(BaseTimestampModel):
+    name=CharField(**NULLABLE_CHARFIELD)
+    image=ImageField(upload_to='products/images/%y/%m/%d', default='products/images/')
+    batch_id=ForeignKey('ProductBatch', on_delete=models.CASCADE)
+
+class ProductBatch(BaseTimestampModel):
+    batch_id=IntegerField(null=True,blank=True)
+    expiry_date=DateField(null=True,blank=True)
+    quantity=models.BigIntegerField(default=0)
+
+
