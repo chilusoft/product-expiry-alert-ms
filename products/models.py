7f1ab1a5e9f8c54d997bd2678a9ef39ec3f2c327
chilusoft <mukukachilu@gmail.com>
2024-12-11 20:04:34 +0200

    Added product and related models

--- products/models.py
+++ products/models.py
@@ -6,18 +6,56 @@ from django.db.models import (
     FloatField,
     ForeignKey,
     ImageField,
-    DateField
+    DateField,
+    DateTimeField,
+    BooleanField,
+    ManyToManyField
 )
 
 
 class Product(BaseTimestampModel):
     name=CharField(**NULLABLE_CHARFIELD)
     image=ImageField(upload_to='products/images/%y/%m/%d', default='products/images/')
-    batch_id=ForeignKey('ProductBatch', on_delete=models.CASCADE)
+    batch=ForeignKey('ProductBatch', on_delete=models.CASCADE)
+    product_category=ManyToManyField('ProductCategory')
 
+    def __str__(self):
+        return f'{self.name} --> {self.batch_id}'
+    
+class ProductCategory(BaseTimestampModel):
+    name=CharField(**NULLABLE_CHARFIELD)
+    image=ImageField(upload_to='products/assets/category/%y/%m/%d',default='products/assets/category/default.png')
 class ProductBatch(BaseTimestampModel):
     batch_id=IntegerField(null=True,blank=True)
     expiry_date=DateField(null=True,blank=True)
+    is_expired=BooleanField(default=False)
     quantity=models.BigIntegerField(default=0)
+    supplier=ForeignKey('Supplier',on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return f'{self.batch_id} is Expired: {self.is_expired}'
+
+
+class Supplier(BaseTimestampModel):
+    store_name = models.CharField(max_length=255)
+    store_location = models.CharField(**NULLABLE_CHARFIELD)
+    store_capacity = models.IntegerField(default=0)
+    store_type = models.CharField(**NULLABLE_CHARFIELD)
+
+    def _str_(self):
+        return self.store_name
+
+
+class ScheduleQueue(BaseTimestampModel):
+    batch = models.OneToOneField(ProductBatch, on_delete=models.CASCADE, related_name="schedule_queue")
+    is_scheduled = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    is_notification_sent = models.BooleanField(default=False)
+    recipient_emails = models.ManyToManyField('index.User', related_name="scheduled_notifications")
+
+    def _str_(self):
+        return f"Schedule Queue for {self.expiration_data.product.product_name}"
+
 
 
