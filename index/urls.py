fc5a985945d99db41f9194b09671eaa7ef299f9f
chilusoft <mukukachilu@gmail.com>
2024-12-13 01:13:45 +0200

    Added Product view and routes

--- index/urls.py
+++ index/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name='index'
 
 urlpatterns=[
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('products/', include('products.urls'))
 ]
